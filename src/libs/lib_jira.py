e7635450a4d7b86aec0503a90e00d6bd11a6586f
MARCELO SANTOS DUARTE <90726831+marcelo-sduarte@users.noreply.github.com>
2024-09-24 13:38:54 +0100

    Update lib_jira.py

--- src/libs/lib_jira.py
+++ src/libs/lib_jira.py
@@ -1,11 +1,13 @@
 import pieces
-from gvars import JIRA_API_TOKEN, JIRA_SERVER, JIRA_USER, PROJECTS
+from gvars import JIRA_ENDPOINT, PROJECTS, PATH_INPUT, TARGET_TOKEN_JIRA
 
 def connect_api_jira(ano, mes):
     try:
         pieces.lib_logging.logger.info(f"[INICIO] connect_api_jira()")
+        # recupera user e token
+        jira_user, jira_token = pieces.lib_process.get_credential(target_name=TARGET_TOKEN_JIRA)
         # Conecta-se ao Jira
-        jira = pieces.JIRA(JIRA_SERVER, basic_auth=(JIRA_USER, JIRA_API_TOKEN))
+        jira = pieces.JIRA(JIRA_ENDPOINT, basic_auth=(jira_user, jira_token))
 
         # Define a data de início do mês anterior e o início deste mês
         primeiro_dia = pieces.datetime.datetime(ano, mes, 1).strftime("%Y-%m-%d")
@@ -20,26 +22,39 @@ def connect_api_jira(ano, mes):
                 'and status = Done').format(primeiro_dia, ultimo_dia)
         
         '''
-        query = f"""
-                ('project in ({PROJECTS}) '
-                 'and issuetype in ( "Debito Tecnico", Story) '
-                 'and resolutiondate >= startOfMonth(-1) and '
-                 'resolutiondate < startOfMonth() and status = Done')
-                """
+        #Montando a String JQL
+        project_str = ', '.join(f'"{project}"' for project in PROJECTS)
+
+        query = (f'project in ({project_str}) '
+                'and issuetype in ( "Debito Tecnico", Story) '
+                'and resolutiondate >= startOfMonth(-1) '
+                'and resolutiondate < startOfMonth() and status = Done')                                            
+
+        # Paginacao pela limitacao de 50 da api Jira
+        start_at = 0
+        max_results = 500
+        issues = []
         
-        # Faz a consulta ao Jira   
-        issues = jira.search_issues(query)
+        while True:
+            batch = jira.search_issues(query, startAt=start_at, maxResults=max_results)
+            if not batch:
+                break
+            issues.extend(batch)
+            start_at += max_results
         # Extrai os dados relevantes das issues
         data = []
         for issue in issues:
             data.append({
             'Key': issue.key,
+            'Project': issue.fields.project,
             'Summary': issue.fields.summary,
+            'Created': issue.fields.created,
             'Resolution Date': issue.fields.resolutiondate
             })
         # Cria um DataFrame a partir dos dados
         df = pieces.pd.DataFrame(data)
-        return df
+        # Salva o DataFrame como um arquivo Excel
+        df.to_excel(PATH_INPUT +'/jira_api.xlsx', index=False, )
     except Exception as error:
         pieces.lib_logging.logger.error(f" > message error connect_api_jira: {error}")
     finally:
