3d463a2dfe91f3d14068c1999bb864fd0cd96723
MARCELO SANTOS DUARTE <90726831+marcelo-sduarte@users.noreply.github.com>
2025-06-17 15:51:17 +0100

    ajuste path

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-CALL C:\Users\Marcelo\Documents\Python\Automation\calculoshoras_jira\env\Scripts\activate.bat
-python C:\Users\Marcelo\Documents\Python\Automation\calculoshoras_jira\src\main.py runserver
+CALL C:\Downloads\calculoshoras_jira\env\Scripts\activate.bat
+python C:\Downloads\calculoshoras_jira\src\main.py runserver
 
 @exit
